cd8dda4c9e296f17f25b272147129b27cc69efca
bhatiaisha6 <124436800+bhatiaisha6@users.noreply.github.com>
2023-02-04 18:02:40 +0530

    Add files via upload
    Week-1 Assignment

---
+++ W1D1/MobileSpecifications.java
@@ -0,0 +1,36 @@
+package assignment.W1D1;
+
+public class MobileSpecifications {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*Assignment2
+		Create a class for Mobile and declare below variables with appropriate values and print the values......
+		String mobileBrandName
+		char mobileLogo
+		short noOfMobilePiece
+		int modelNumber
+		long mobileImeiNumber
+		float mobilePrice
+		boolean isDamaged*/
+		
+		String mobileBrandName = "Apple";
+		char mobileLogo = 'A';
+		short noOfMobilePiece = 102;
+		int modelNumber = 13;
+		long mobileImeiNumber = 67876567656756L;
+		float mobilePrice = 120000.99f;	
+		boolean isDamaged = false;
+		
+		System.out.println("This is the output of Assignment-2");
+		System.out.println("This is the mobile brand name : "+mobileBrandName);
+		System.out.println("This is the mobile Logo : "+mobileLogo);
+		System.out.println("This is the number of Mobile pieces : "+noOfMobilePiece);
+		System.out.println("Mobile number is : "+mobileImeiNumber);
+		System.out.println("The price of mobile is : "+mobilePrice+" INR");
+		System.out.println("IS the mobile damaged? : "+isDamaged);
+
+	}
+
+}
---
+++ W1D1/TwoWheeler.java
@@ -0,0 +1,25 @@
+package assignment.W1D1;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		
+		int noOfwheels = 12;
+		short noOfMirrors = 2;
+		long chassisNumber = 67898778976789L;
+		boolean isPunctured = false;
+		String bikeName = "Harley";
+		double runningKM = 100050.67677474;
+		
+		System.out.println("This is the output of Assignment-1");
+		System.out.println("This is the number of mirrors: "+noOfMirrors);
+		System.out.println("This is the chassisNumber: "+chassisNumber);
+		System.out.println("Is the bike punctured? : "+isPunctured);
+		System.out.println("Name of the bike is: "+bikeName);
+		System.out.println("Bike has "+noOfwheels+" wheels");
+		System.out.println("Bike has run "+runningKM+" KMs");
+
+		
+	}
+
+}
